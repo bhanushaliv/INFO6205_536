6678890642b0535dab3e1376feee767620a3fc7c
Shrikant Mudholkar <mudholkar.s@husky.neu.edu>
2018-04-14 13:46:50 -0400

    Updating Driver

--- src/edu/neu/genetic/algorithm/Driver.java
+++ src/edu/neu/genetic/algorithm/Driver.java
@@ -1,21 +1,6 @@
 package edu.neu.genetic.algorithm;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Driver {
-
-    List<City> initialRoute = new ArrayList<>(Arrays.asList(new City("Boston", 42.3601, -71),
-            new City("Austin", 30.26, -97),
-            new City("Houston", 29.7604, -95.3698),
-            new City("San Francisco", 37, -122),
-            new City("Denver", 39, -104),
-            new City("Los Angeles", 34, -118),
-            new City("Chicago", 41.3601, -87),
-            new City("Dallas", 32, -96),
-            new City("New York", 40.3601, -74)));
-
 
     public static void main(String[] args) {
 
